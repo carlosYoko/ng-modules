7f5ad2bd62a3b658565691ac320ad68359377bd6
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-09-18 22:22:28 +0200

    refactor

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
-import { ListCoursesComponent } from './list-courses/list-courses.component';
+import { ListCoursesComponent } from './pages/list-courses/list-courses.component';
 
 
 @NgModule({
